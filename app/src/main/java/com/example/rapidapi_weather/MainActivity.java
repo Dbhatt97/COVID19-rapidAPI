84f6402c5553d29ab100ea85fea6830488be28fa
Dbhatt97 <dkbhatt1997@gmail.com>
2020-09-26 13:06:26 +0530

    Map Linked With Countries 0.0.3

--- app/src/main/java/com/example/rapidapi_weather/MainActivity.java
+++ app/src/main/java/com/example/rapidapi_weather/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.rapidapi_weather;
 
 import android.graphics.Color;
-import android.location.Location;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,21 +13,15 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -33,6 +29,8 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -40,15 +38,20 @@ import okhttp3.Response;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
-   public static TextView show,Confirm_case,Active_case,Death_case,Recovered_case;
+
+    private static final String TAG = MainActivity.class.getName();
+
+    public static TextView show,Confirm_case,Active_case,Death_case,Recovered_case;
     String name;
     Double lat,lon;
+    int flag;
     public  Spinner spinner;
     String responseStr;
     ArrayList<String> countrylist;
     private RequestQueue mqueue;
     private GoogleMap mMap;
     SupportMapFragment mapFragment;
+    private static final float DEFAULT_ZOOM = 5f;
 
 
     @Override
@@ -67,10 +70,6 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             spinner = findViewById(R.id.countrylist);
             countrylist = new ArrayList<>();
 
-
-
-
-
             new getdata().execute();
             new LatestNews().execute();
 
@@ -81,6 +80,9 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                     ((TextView) parent.getChildAt(0)).setTextColor(Color.WHITE);
                     String country=   spinner.getItemAtPosition(spinner.getSelectedItemPosition()).toString();
                     show.setText(country);
+                    flag = countrylist.indexOf(country);
+                    Log.d("MYINT", "value: " + flag); //loging index value to get lat log of that city
+                    geoLocate();
                 }
 
                 @Override
@@ -92,32 +94,19 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
          mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
 
 
-        }
+    }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        float zoom = 5.0f;
         mMap = googleMap;
         // Add a marker in Sydney and move the camera
-        if(lat==null && lon==null) {
-            Location location = null;
-            Double latti = location.getLatitude();
-            Double longii = location.getLongitude();
-            LatLng selected_country = new LatLng(latti, longii);
-            mMap.addMarker(new
-                    MarkerOptions().position(selected_country).title("Your Area"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(selected_country,zoom));
-
-        }
-        else {
-            Toast.makeText(MainActivity.this,"poop",Toast.LENGTH_LONG).show();
-
-        }
-
-
+        LatLng TutorialsPoint = new LatLng(21, 57);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(TutorialsPoint));
     }
 
 
@@ -166,18 +155,44 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                     name = (String) jo.get("name");
                     countrylist.add(name);
 
-                    lat = jo.isNull("latitude") ? null : jo.getDouble("latitude");
-                    lon = jo.isNull("longitude") ? null : jo.getDouble("longitude");
                 }
                 catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
-           // Toast.makeText(MainActivity.this,String.valueOf(lat),Toast.LENGTH_LONG).show();
+
+            // Toast.makeText(MainActivity.this,String.valueOf(lat),Toast.LENGTH_LONG).show();
             spinner.setAdapter(new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_spinner_dropdown_item,countrylist));
+
 
             super.onPostExecute(avoid);
 
+        }
+    }
+
+    private void geoLocate(){
+
+        String searchString = countrylist.get(flag);
+        Geocoder geocoder = new Geocoder(MainActivity.this);
+        List<Address> list = new ArrayList<>();
+        try{
+            list = geocoder.getFromLocationName(searchString, 1);
+        }
+        catch (IOException e){
+            Log.d(TAG,"geoLocate: IOException: " + e.getMessage());
+        }
+
+        if(list.size() > 0){
+            Address address = list.get(0);
+
+            moveCamera(new LatLng(address.getLatitude(),address.getLongitude()),DEFAULT_ZOOM);
+       
+        }
+    }
+    private void moveCamera(LatLng latLng, float zoom){
+
+         if(mMap!=null) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
         }
     }
 }
